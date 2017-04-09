3f8b08bb34f22027b91f84730352b0dd277cf19e
Elegenthus <996886231@qq.com>
2017-04-09 15:23:46 +0800

    update

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,20 +1,32 @@
 <template>
-	<div class="background">
-		<div class="test"></div>
-	</div>
+    <div class="background">
+        <div class="test">
+        	<purse></purse>
+        </div>
+    </div>
 </template>
-
-<style lang = 'sass'>
+<script>
+import Purse from './purse.vue'
+export default {
+    components: {
+        "purse": Purse,
+    },
+}
+</script>
+<style lang='sass'>
 @import '../spritesmith-generated/sprite.scss';
 @import '../scss/common.scss';
-.background{
-	width:100%;
-	height: 100%;
+.background {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
+
 .test {
-  	@include _sprite($background,1156px);
-  	height: y_size(1156);
-  	width: 100%;
-  	margin-top: 30px;
+    @include _sprite($background, 1156px);
+    height: y_size(1156);
+    width: 100%;
+    margin-top: 30px;
+    height: 100%;
 }
 </style>
---
+++ src/components/purse.vue
@@ -0,0 +1,8 @@
+<template>
+	<div>
+		<slot>this is purse</slot>
+	</div>
+</template>
+<script>
+	
+</script>
